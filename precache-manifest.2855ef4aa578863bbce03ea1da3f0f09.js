d1cc6a6ef58675e105b8de53200a38edce9641b5
Gabriel Cioci <ciocigabi@gmail.com>
2020-02-29 23:50:04 +0200

    Updates

---
+++ precache-manifest.2855ef4aa578863bbce03ea1da3f0f09.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d60f9cab185a24823b3f62fc359950c",
+    "url": "/slot-machine/index.html"
+  },
+  {
+    "revision": "949ce1c40a4978ce901a",
+    "url": "/slot-machine/static/css/main.1c1e6e1c.chunk.css"
+  },
+  {
+    "revision": "c2d7cd959e92275f9317",
+    "url": "/slot-machine/static/js/2.d58f6d96.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/slot-machine/static/js/2.d58f6d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949ce1c40a4978ce901a",
+    "url": "/slot-machine/static/js/main.7ee9f2e4.chunk.js"
+  },
+  {
+    "revision": "3c93d56618efbf3abb00",
+    "url": "/slot-machine/static/js/runtime-main.87ce03ab.js"
+  },
+  {
+    "revision": "68885c6dcbf2c13f8ac3cd6866b8053c",
+    "url": "/slot-machine/static/media/lilliputsteps.68885c6d.ttf"
+  },
+  {
+    "revision": "9044b4df1a9963ec5a0b7c6af03f861c",
+    "url": "/slot-machine/static/media/lose.9044b4df.mp3"
+  },
+  {
+    "revision": "c3f2a53732c985db2fb7abdfc3aed9b9",
+    "url": "/slot-machine/static/media/spin.c3f2a537.mp3"
+  },
+  {
+    "revision": "eaa618d217b5842c5295ddf9f9811996",
+    "url": "/slot-machine/static/media/win.eaa618d2.mp3"
+  }
+]);
